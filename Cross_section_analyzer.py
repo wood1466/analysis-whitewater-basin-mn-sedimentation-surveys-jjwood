a46bfe1f8b360f95ca85748e9537bf10e61b0549
wood1466 <wood1466@umn.edu>
2023-02-16 16:21:21 -0600

    Modified DataFrame cell slicer and plot lines. Duplicated functions in program by copy and cut from primary—no real reason.

--- Cross_section_analyzer.py
+++ Cross_section_analyzer.py
@@ -113,9 +113,10 @@ def slice_DataFrame_rows2(dataframe, column1, row_value1, label1, row_value2, la
               '\n..\n', df_slc_r, '\n')  # Displays objects.
     return df_slc_r  # Ends execution of function.
 
-def slice_DataFrame_cell(dataframe, column, index, data_label,
-                         dsply):  # Defines function. For DataFrame slicing by row value.
-    df_slc_cl = dataframe.loc[index, column]  # Defines function format.
+
+def slice_DataFrame_cell(dataframe, column, data_label, dsply):  # Defines function. For DataFrame slicing by row value.
+    index = dataframe.index
+    df_slc_cl = dataframe.loc[index[0], column]  # Defines function format.
     if dsply == 1:  # Conditional statement. For display.
         print(data_label + ':', df_slc_cl, '\n')  # Displays objects.
     return df_slc_cl  # Ends execution of function.
@@ -227,3 +228,203 @@ def get_marker(label):
     elif label == '1850s':  # Conditional statement. For display.
         marker = mrkr_mpltlib[-2]  # Defines variable. Sets plot marker.
     return marker
+
+
+tol_mtd = ['#332288', '#88CCEE', '#44AA99', '#117733', '#999933', '#DDCC77', '#CC6677', '#882255', '#AA4499', '#DDDDD']  # Defines list. Sets Paul Tol muted colorblind friendly palette via hex color codes.
+
+# TEMPORARY FUNCTION HOUSING ===========================================================================================
+#MOST RECENT YADA YDA RODDA RODA
+
+def plot_lines(lines, plot_number, figure_size, x, y, label, color, marker, marker_size, line_width,
+               alpha, show_legend, location, marker_scale, frame_alpha, label_spacing, fontsize_ticks, x_label,
+               fontsize_axis, label_pad, y_label, title, pause,
+               pause_length):  # Defines function. For cross-section plotting.
+    plt.figure(plot_number, figsize=figure_size)  # Creates plot window.
+    ax = plt.gca()  # Defines variable. Retrieves plot axes instance.
+    if lines == 1:  # Conditional statement. Plots single line.
+        ax.plot(x, y, label=label, c=color, marker=marker, markersize=marker_size, linewidth=line_width,
+                alpha=alpha)  # Creates line plot of arrays from axes instance. Sets format.
+        if show_legend == 1: # Conditional statement. Shows legend if desired for single line plotting.
+            ax.legend(loc=location, markerscale=marker_scale, framealpha=frame_alpha,
+                      labelspacing=label_spacing)  # Creates legend. Through automatic label detection.
+    if lines != 1:  # Conditional statement. Plots multiple lines.
+        lines = lines + 1  # Defines variable. For correct looping framework.
+        line_list = range(1, lines, 1)  # Defines list. Creates range of integers for looped plotting.
+        for i in line_list:  # Begins loop through list. For sequential plotting.
+            index = line_list.index(i)  # Defines variable. Retrieves index of element in list. For format selection.
+            ax.plot(x[index], y[index], label=label[index], c=color, marker=marker, markersize=marker_size,
+                    linewidth=line_width,
+                    alpha=alpha)  # Creates line plot of arrays from axes instance. Sets format.
+        ax.legend(loc=location, markerscale=marker_scale, framealpha=frame_alpha,
+                  labelspacing=label_spacing)  # Creates legend. Through automatic label detection.
+    plt.xticks(fontsize=fontsize_ticks)  # Sets x-axis tick mark format.
+    plt.xlabel(x_label, fontsize=fontsize_axis, labelpad=label_pad)  # Creates x-axis label. Sets format.
+    plt.ylabel(y_label, fontsize=fontsize_axis)  # Creates x-axis label. Sets format.
+    plt.yticks(fontsize=fontsize_ticks)  # Sets y-axis tick mark format.
+    plt.title(title)  # Creates plot title.
+    if pause == 1:  # Conditional statement. For display format.
+        plt.pause(pause_length)  # Displays plot. For set interval of seconds and closes without clearing.
+    elif pause == 0:  # Conditional statement. For displa format.
+        plt.show()  # Displays plot. Indefinite and cleared upon close.
+
+def create_folder(level, path):  # Defines function. For generating directory paths.
+    if not os.path.exists(path):  # Checks if folder exists. Skips step if exists.
+        os.mkdir(path)  # Creates folder if it does not exist.
+        print('New directory level', level, '\033[0;32m' + path + '\033[0m', 'created')  # Displays objects.
+
+def upload_csv(path, label, dsply):  # Defines function. For uploading .csv and conversion to DataFrame.
+    csv_data = pd.read_csv(path)  # Uploads .csv file. Input data.
+    df = pd.DataFrame(csv_data)  # Converts .csv file to DataFrame. For Python manipulation.
+    pd.set_option('display.max_columns', None)  # Adjusts DataFrame format. Displays all DataFrame columns.
+    if dsply == 1:  # Conditional statement. For display.
+        print('\033[1m' + 'UPLOADED .CSV DATA FOR ' + label + '\033[0m', '\n...\n', df, '\n')  # Displays objects.
+    return df  # Ends execution of function.
+
+def forward_range(start, end, step, data_label, dsply):  # Defines function. For generating forward range array between two numbers.
+    end = end + 1  # Defines variable. Sets end of range to include final input value.
+    end_label = end - 1  # Defines variable. Sets label for display.
+    frwd_rng = np.arange(start, end, step)  # Defines function format.
+    if dsply == 1:  # Conditional statement. For display.
+        print(data_label, start, '&', end_label, ':', frwd_rng)  # Displays objects.
+    return frwd_rng  # Ends execution of function.
+
+def reverse_range(start, end, step, data_label, dsply):  # Defines function. For generating forward range array between two numbers.
+    end = end - 1  # Defines variable. Sets end of range to include final input value.
+    end_label = end + 1  # Defines variable. Sets label for display.
+    rev_rng = np.arange(start, end, step)  # Defines function format.
+    if dsply == 1:  # Conditional statement. For display.
+        print(data_label, start, '&', end_label, ':', rev_rng, '\n')  # Displays objects.
+    return rev_rng  # Ends execution of function.
+
+def slice_DataFrame_rows1(dataframe, column, row_value, label, dsply):  # Defines function. For DataFrame slicing by single row value.
+    df_slc_r = dataframe[dataframe[column] == row_value]  # Defines function format.
+    if dsply == 1:  # Conditional statement. For display.
+        print('\033[1m' + label + ' ' + str(row_value) + ' DATA' + '\033[0m', '\n..\n', df_slc_r, '\n')  # Displays objects.
+    return df_slc_r  # Ends execution of function.
+
+def slice_DataFrame_rows2(dataframe, column1, row_value1, label1, row_value2, label2,
+                          dsply):  # Defines function. For DataFrame slicing by row value.
+    df_slc_r = dataframe[dataframe[column1] == row_value1]  # Defines function format.
+    if dsply == 1:  # Conditional statement. For display.
+        print('\033[1m' + label2 + ' ' + str(row_value2) + ' ' + label1 + ' ' + str(row_value1) + ' DATA' + '\033[0m',
+              '\n..\n', df_slc_r, '\n')  # Displays objects.
+    return df_slc_r  # Ends execution of function.
+
+
+def slice_DataFrame_cell(dataframe, column, data_label, dsply):  # Defines function. For DataFrame slicing by row value.
+    index = dataframe.index
+    df_slc_cl = dataframe.loc[index[0], column]  # Defines function format.
+    if dsply == 1:  # Conditional statement. For display.
+        print(data_label + ':', df_slc_cl, '\n')  # Displays objects.
+    return df_slc_cl  # Ends execution of function.
+
+def slice_DataFrame_columns1(dataframe, column, data_label,
+                             dsply):  # Defines function. For DataFrame slicing by row value.
+    df_slc_c = dataframe[column]  # Defines function format.
+    if dsply == 1:  # Conditional statement. For display.
+        print('\033[1m' + data_label + ' DATA' + '\033[0m', '\n..\n', df_slc_c, '\n')  # Displays objects.
+    return df_slc_c  # Ends execution of function.
+
+def max_value_column(dataframe, data_label, units,
+                     dsply):  # Defines function. For retrieving maximum value from DataFrame column.
+    max = dataframe.max()  # Defines function format.
+    if dsply == 1:  # Conditional statement. For display.
+        print('Maximum column value:', max, data_label, units)  # Displays objects.
+    return max  # Ends execution of function.
+
+def min_value_column(dataframe, data_label, units,
+                     dsply):  # Defines function. For retrieving minimum value from DataFrame column.
+    min = dataframe.min()  # Defines function format.
+    if dsply == 1:  # Conditional statement. For display.
+        print('Minimum column value:', min, data_label, units)  # Displays objects.
+    return min  # Ends execution of function.
+
+
+def hydraulic_geometry(dataframe, column1, data_label1, data_label2, units, data_label3, data_label4,
+                       column2, data_label5, data_label6, data_label7, data_label8, dsply):  # Defines
+    # function. For hydraulic geometry calculation.
+
+    # Calculate channel width
+    df_strm_offst = slice_DataFrame_columns1(dataframe, column1, data_label1, 0)  # Defines DataFrame.
+    # Slices DataFrame to contain stream channel survey offsets only.
+    offst1 = min_value_column(df_strm_offst, data_label2, units, 0)  # Defines variable. Slices DataFrame
+    # to yield smallest offset for stream channel.
+    offst2 = max_value_column(df_strm_offst, data_label2, units, 0)  # Defines variable. Slices DataFrame
+    # to yield largest offset for stream channel.
+    if offst1 < offst2:  # Conditional statement. Sets order of operations for channel width calculation.
+        wdth = offst2 - offst1  # Defines variable. Sets calculation framework.
+    elif offst1 > offst2:  # Conditional statement. Sets error contingency.
+        sys.exit('No stream channel detected')  # Exits code and displays string.
+    elif offst1 == offst2:  # Conditional statement. Sets error contingency.
+        sys.exit('No stream channel detected')  # Exits code and displays string.
+    if dsply == 1:  # Conditional statement. For display.
+        print(data_label3 + units + '\n  ' + data_label4 + '%.1f' % wdth)  # Displays objects.
+
+    # Calculate channel depth
+    df_strm_elvtn = slice_DataFrame_columns1(dataframe, column2, data_label5, 0)  # Defines DataFrame.
+    # Slices DataFrame to contain stream channel survey offsets only.
+    elvtn1 = min_value_column(df_strm_elvtn, data_label6, units, 0)  # Defines variable. Slices DataFrame
+    # to yield smallest elevation for stream channel.
+    elvtn2 = max_value_column(df_strm_elvtn, data_label6, units, 0)  # Defines variable. Slices DataFrame
+    # to yield largest elevation for stream channel.
+    if elvtn1 < elvtn2:  # Conditional statement. Sets order of operations for channel depth calculation.
+        dpth = elvtn2 - elvtn1  # Defines variable. Sets calculation framework.
+    elif elvtn1 > elvtn2:  # Conditional statement. Sets error contingency.
+        sys.exit('No stream channel detected')  # Exits code and displays string.
+    elif elvtn1 == elvtn2:  # Conditional statement. Sets error contingency.
+        sys.exit('No stream channel detected')  # Exits code and displays string.
+    elif elvtn1 | elvtn2 <= 0:  # Conditional statement. Sets error contingency.
+        sys.exit('No stream channel detected')  # Exits code and displays string.
+    if dsply == 1:  # Conditional statement. For display.
+        print('  ' + data_label7 + '%.2f' % dpth)  # Displays objects.
+
+    # Calculate hydraulic radius
+    hydro_rad = (wdth * dpth) / (wdth + 2 * dpth)  # Defines variable. Sets calculation framework.
+    if dsply == 1:  # Conditional statement. For display.
+        print('  ' + data_label8 + '%.2f' % hydro_rad)  # Displays objects.
+
+    return wdth, dpth, hydro_rad  # Ends execution of function.
+
+
+def create_appended_list(value, data_label1, new_list, data_label2,
+                         dsply):  # Defines function. For post calculation list collection.
+
+    try:  # Checks program for object. Takes action based off existence.
+        value  # Defines object to be searched for.
+    except NameError:  # Executed if object does not exist.
+        sys.exit(data_label1 + 'value undefined')
+    else:  # Executed if object does exist.
+        new_list.append(value)  # Appends list.
+    if dsply == 1:  # Conditional statement. For display.
+        print(data_label2, new_list)  # Displays objects.
+    return new_list  # Ends execution of function.
+
+
+def get_plot_format_by_year(label, list, alternate):  # Defines function. For plot color selection.
+    if label == '2008':  # Conditional statement.
+        quality = list[0]  # Defines variable. Sets plot color.
+        if alternate == 1:  # Conditional statement.
+            quality = list[-3]  # Defines variable. Sets plot color.
+    elif label == '1994':  # Conditional statement.
+        quality = list[3]  # Defines variable. Sets plot color.
+    elif label == '1978':  # Conditional statement.
+        quality = list[2]  # Defines variable. Sets plot color.
+    elif label == '1975':  # Conditional statement.
+        quality = list[4]  # Defines variable. Sets plot color.
+    elif label == '1964':  # Conditional statement.
+        quality = list[5]  # Defines variable. Sets plot color.
+    elif label == '1939':  # Conditional statement.
+        quality = list[6]  # Defines variable. Sets plot color.
+    elif label == '1850s':  # Conditional statement.
+        quality = list[7]  # Defines variable. Sets plot color.
+    elif label == 'Shaded area':  # Conditional statement.
+        quality = list[-1]  # Defines variable. Sets plot color.
+    return quality  # Ends execution of function.
+
+
+def create_DataFrame_frm_arry(array1, array2, data_label1,
+                              dsply):  # Defines function. For creating DataFrame from arrays or lists.
+    df_new = pd.DataFrame(data=array1, columns=array2)  # Defines function format.
+    if dsply == 1:  # Conditional statement. For display.
+        print('\033[1m' + data_label1 + ' DATA' + '\033[0m', '\n..\n', df_new, '\n')  # Displays objects.
+    return df_new  # Ends execution of function.
