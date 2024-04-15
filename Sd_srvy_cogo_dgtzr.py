969fb378aae550230df44199286ef045f785c82b
wood1466 <wood1466@umn.edu>
2024-04-15 13:40:37 -0500

    Changed DataFrame slicer columns to match new naming system in spreadsheets. Added third file input to separate monument coordinates from monument metadata.

--- Sd_srvy_cogo_dgtzr.py
+++ Sd_srvy_cogo_dgtzr.py
@@ -24,15 +24,14 @@ otpt_fldr = 'Output'  # Defines variable. Sets name of output folder where all d
 gis_fldr = 'GIS'  # Defines variable. Sets name of output folder where all geospatial data products will be exported.
 
 # Data
-inpt_fl1 = inpt_fldr + '/Survey data new MS-MF.csv'  # Defines variable. Sets file path to input file.
-inpt_fl2 = inpt_fldr + '/Monument metadata original.csv'  # Defines variable. Sets file path to input file.
+inpt_fl1 = inpt_fldr + '/Whitewater sedimentation survey data 4 15.csv'  # Defines variable. Sets file path to input
+# file.
+inpt_fl2 = inpt_fldr + '/Monument metadata semifinal 4 11.csv'  # Defines variable. Sets file path to input file.
+inpt_fl3 = inpt_fldr + '/Monument coordinates semifinal 4 15.csv'  # Defines variable. Sets file path to input file.
 
 # Limits of analysis
 rng_strt = 1  # Defines variable. Sets start survey range number for operation loop.
-rng_end = 26  # Defines variable. Sets end survey range number for operation loop.
-
-# Data slicing criteria
-crdnt_srvy_yr = 2011  # Defines variable. Sets survey year of field coordinate survey.
+rng_end = 107  # Defines variable. Sets end survey range number for operation loop.
 
 # Conversion factors
 ft_to_m = 3.281  # Defines variable. Sets conversion factor from feet to meters.
@@ -50,7 +49,8 @@ create_folder(otpt_fldr + '/' + gis_fldr)  # Creates folder. Calls function. Cre
 # UPLOAD FILE(S) -------------------------------------------------------------------------------------------------------
 
 df_srvy_dt = csv_to_DataFrame(inpt_fl1, 0)  # Defines DataFrame. Calls function. Uploads survey data.
-df_mnt_dt = csv_to_DataFrame(inpt_fl2, 0)  # Defines DataFrame. Calls function. Uploads monument metadata.
+df_mnt_mtdt = csv_to_DataFrame(inpt_fl2, 0)  # Defines DataFrame. Calls function. Uploads monument metadata.
+df_crdnt_dt = csv_to_DataFrame(inpt_fl3, 0)  # Defines DataFrame. Calls function. Uploads monument coordinate data.
 
 # ======================================================================================================================
 # PART 2: DATA OPERATIONS ----------------------------------------------------------------------------------------------
@@ -67,56 +67,68 @@ rng_nmbrs = forward_range(rng_strt, rng_end, 1, 0)  # Defines array. Calls funct
 for i in rng_nmbrs:  # Begins loop. Loops through array elements. Loops through transect numbers. Calculates coordinates
     # for each range in sequence.
 
-    df_rng_dt = slice_DataFrame_rows('Equals', df_srvy_dt, 'Range_num', i, 0)  # Defines DataFrame. Calls function.
+    df_rng_dt = slice_DataFrame_rows('Equals', df_srvy_dt, 'range_number', i, 0)  # Defines DataFrame. Calls function.
     # Slices DataFrame to yield single range data.
 
     # Metadata
-    df_rng_mtdt = slice_DataFrame_rows('Equals', df_mnt_dt, 'Range_num', str(i), 0)  # Defines DataFrame. Calls
+    df_rng_mtdt = slice_DataFrame_rows('Equals', df_mnt_mtdt, 'range_number', i, 0)  # Defines DataFrame. Calls
     # function. Slices DataFrame to yield single range metadata.
 
     # Select monument data ---------------------------------------------------------------------------------------------
 
-    df_mnt_crdnts = slice_DataFrame_rows('Equals', df_rng_mtdt, 'Srvy_year', crdnt_srvy_yr, 0)  # Defines DataFrame.
-    # Calls function. Slices DataFrame to yield single range monument coordinate data.
+    df_mnt_crdnts = slice_DataFrame_rows('Equals', df_crdnt_dt, 'range_number', i, 0)  # Defines DataFrame. Calls
+    # function. Slices DataFrame to yield single range monument coordinate data.
 
-    # Coordinates
-    mnt1_e = slice_DataFrame_cell('Float', df_mnt_crdnts, 0, 'Easting', 0)  # Defines variable. Calls function.
-    # Slices DataFrame to yield Easting coordinate of starting monument of present dataset.
-    mnt1_n = slice_DataFrame_cell('Float', df_mnt_crdnts, 0, 'Northing', 0)  # Defines variable. Calls function.
-    # Slices DataFrame to yield Northing coordinate of starting monument of present dataset.
-    mnt2_e = slice_DataFrame_cell('Float', df_mnt_crdnts, 1, 'Easting', 0)  # Defines variable. Calls function.
-    # Slices DataFrame to yield Easting coordinate of ending monument of present dataset.
-    mnt2_n = slice_DataFrame_cell('Float', df_mnt_crdnts, 1, 'Northing', 0)  # Defines variable. Calls function.
-    # Slices DataFrame to yield Northing coordinate of ending monument of present dataset.
+    if len(df_mnt_crdnts.axes[0]) != df_mnt_crdnts.isnull().sum().sum():  # Begins conditional statement. Checks
+    # inequality.
+        # Retrieve coordinates for ranges where coordinates exist only.
+        # Coordinates
+        mnt1_e = slice_DataFrame_cell('Float', df_mnt_crdnts, 0, 'easting_m', 0)  # Defines variable. Calls function.
+        # Slices DataFrame to yield Easting coordinate of starting monument of present dataset.
+        mnt1_n = slice_DataFrame_cell('Float', df_mnt_crdnts, 0, 'northing_m', 0)  # Defines variable. Calls function.
+        # Slices DataFrame to yield Northing coordinate of starting monument of present dataset.
+        mnt2_e = slice_DataFrame_cell('Float', df_mnt_crdnts, 1, 'easting_m', 0)  # Defines variable. Calls function.
+        # Slices DataFrame to yield Easting coordinate of ending monument of present dataset.
+        mnt2_n = slice_DataFrame_cell('Float', df_mnt_crdnts, 1, 'northing_m', 0)  # Defines variable. Calls function.
+        # Slices DataFrame to yield Northing coordinate of ending monument of present dataset.
 
-    # Reference station
-    mnt1_stn = slice_DataFrame_cell('Float', df_mnt_crdnts, 0, 'BM_off_tp', 0)
+        # Reference station
+        mnt1_stn = slice_DataFrame_cell('Float', df_mnt_crdnts, 0, 'marker_station', 0)  # Defines variable. Calls
+        # function.  Slices DataFrame to yield survey station (range position) of starting monument.
+
+        # Check for shift in monument coordinates ----------------------------------------------------------------------
+
+        mnt1_e, mnt1_n = check_marker_shift(mnt1_e, mnt1_n, df_mnt_crdnts, 0, ft_to_m, 0)  # Redefines variables. Calls
+        # function. Checks if coordinates need shifted to account for monument offset from range line.
+        mnt2_e, mnt2_n = check_marker_shift(mnt2_e, mnt2_n, df_mnt_crdnts, 1, ft_to_m, 0)  # Redefines variables. Calls
+        # function. Checks if coordinates need shifted to account for monument offset from range line.
 
     # Select survey data -----------------------------------------------------------------------------------------------
 
-    srvy_nmbrs = slice_DataFrame_columns('Array', 'Integer', df_rng_dt, 'Srvy_num', 1, 0, 0)  # Defines array. Calls
-    # function. Slices DataFrame to yield survey numbers of range dataset.
+    srvy_nmbrs = slice_DataFrame_columns('Array', 'Integer', df_rng_dt, 'survey_number', 1, 0, 0)  # Defines array.
+    # Calls function. Slices DataFrame to yield survey numbers of range dataset.
 
     for j in srvy_nmbrs:  # Begins loop. Loops through array elements. Loops through survey numbers. Calculates
         # coordinates for each survey in sequence.
-        df_rng_srvy_dt = slice_DataFrame_rows('Equals', df_rng_dt, 'Srvy_num', j, 0)  # Defines DataFrame. Calls
+        df_rng_srvy_dt = slice_DataFrame_rows('Equals', df_rng_dt, 'survey_number', j, 0)  # Defines DataFrame. Calls
         # function. Slices DataFrame to yield single range single survey data.
 
         # Metadata
-        df_rng_srvy_mtdt = slice_DataFrame_rows('Equals', df_rng_mtdt, 'Srvy_num', j, 0)  # Defines DataFrame. Calls
-        # function. Slices DataFrame to yield single range survey metadata.
+        df_rng_srvy_mtdt = slice_DataFrame_rows('Equals', df_rng_mtdt, 'survey_number', j, 0)  # Defines DataFrame.
+        # Calls function. Slices DataFrame to yield single range survey metadata.
 
         retrieve_metadata(df_rng_srvy_mtdt, 1)  # Calls function. Displays and retrieves metadata.
 
         # Survey data
-        df_stns = slice_DataFrame_columns('DataFrame', 'Float', df_rng_srvy_dt, 'Offset_ft', 0, 0, 0)  # Defines array.
+        df_stns = slice_DataFrame_columns('DataFrame', 'Float', df_rng_srvy_dt, 'station', 0, 0, 0)  # Defines array.
         # Calls function. Slices DataFrame to yield survey stations of survey dataset.
-        df_elvtns = slice_DataFrame_columns('DataFrame', 'Float', df_rng_srvy_dt, 'Elv_geo_ft', 0, 0, 0)  # Defines
+        df_elvtns = slice_DataFrame_columns('DataFrame', 'Float', df_rng_srvy_dt, 'elevation_1', 0, 0, 0)  # Defines
         # array. Calls function. Slices DataFrame to yield survey elevations of survey dataset.
 
         # CALCULATE STATION COORDINATE GEOMETRY ------------------------------------------------------------------------
+
         if len(df_elvtns.axes[0]) != df_elvtns.isnull().sum().sum():  # Begins conditional statement. Checks inequality.
-            # Digitizes all data that has elevation records. Ignores soil bore depth.
+            # Digitizes all data that has elevation records. Ignores soil bore depth only data.
             # Station separations down range
             df_dlt_r = df_stns - mnt1_stn  # Defines DataFrame. Calculates station separation between survey points and
             # reference monument.
@@ -135,19 +147,19 @@ for i in rng_nmbrs:  # Begins loop. Loops through array elements. Loops through 
             df_e = (df_dlt_e / ft_to_m) + mnt1_e  # Defines DataFrame. Calculates Easting coordinate for all stations.
             df_n = (df_dlt_n / ft_to_m) + mnt1_n  # Defines DataFrame. Calculates Northing coordinate for all stations.
 
-            df_e = df_e.rename('Easting')  # Redefines DataFrame. Renames column.
-            df_n = df_n.rename('Northing')  # Redefines DataFrame. Renames column.
+            df_e = df_e.rename('easting_m')  # Redefines DataFrame. Renames column.
+            df_n = df_n.rename('northing_m')  # Redefines DataFrame. Renames column.
 
             # Plot
-            # plt.scatter(df_e, df_n, c='Cyan', alpha=0.1)  # Creates scatter plot. Plots station coordinates. Enable for
-            # check.
-            # plt.scatter([mnt1_e, mnt2_e], [mnt1_n, mnt2_n], c='Orange')  # Creates scatter plot. Plots monument
-            # coordinates. Enable for check.
-            # plt.pause(0.1)  # Plot pause command. Enables continuous plotting on same figure at interval of 1 second.
+            # plt.scatter(df_e, df_n, c='c', alpha=0.1)  # Creates scatter plot. Plots station coordinates. Enable for
+            # # check.
+            # plt.scatter([mnt1_e, mnt2_e], [mnt1_n, mnt2_n], c='m', alpha = 0.9)  # Creates scatter plot. Plots monument
+            # # coordinates. Enable for check.
+            # plt.pause(1)  # Plot pause command. Enables continuous plotting on same figure at interval.
 
-            # PREPARE DATA FOR DIGITIZATION --------------------------------------------------------------------------------
+            # PREPARE DATA FOR DIGITIZATION ----------------------------------------------------------------------------
 
-            # Compile calculations -----------------------------------------------------------------------------------------
+            # Compile calculations -------------------------------------------------------------------------------------
 
             try:  # Begins try-except statement. Checks object existence. Executes code when existence satisfied.
                 df_e_all = pd.concat([df_e_all, df_e], axis=0)  # Redefines DataFrame. Concatenates DataFrames if it
@@ -166,23 +178,23 @@ for i in rng_nmbrs:  # Begins loop. Loops through array elements. Loops through 
         # Update data file ---------------------------------------------------------------------------------------------
 
         if i == rng_end:  # Begins conditional statement. Checks equality. Executes code when condition satisfied.
-            # Saves coordinates to DataFrame and digitizes DataFrame when all surveys have been completed.
+            # Save coordinates to DataFrame and digitizes DataFrame when all surveys have been completed.
             if j == srvy_nmbrs[-1]:  # Begins conditional statement. Checks equality. Executes code when condition
                 # satisfied.
                 df_srvy_dt = pd.concat([df_srvy_dt, df_e_all, df_n_all], axis=1)  # Redefines DataFrame.
-                # Concatenates DataFrames. Appends coordinates to DataFrame for digitization.
+                # Concatenates DataFrames. Append coordinates to DataFrame for digitization.
 
-                srvy_eras = slice_DataFrame_columns('Array', 'Integer', df_srvy_dt, 'Srvy_year', 1, 0, 0)  # Defines
+                srvy_eras = slice_DataFrame_columns('Array', 'Integer', df_srvy_dt, 'survey_era', 1, 0, 0)  # Defines
                 # array. Calls function. Slices DataFrame to yield survey eras of complete dataset.
 
                 for k in srvy_eras:  # Begins loop. Loops through survey era years. Digitizes data for each survey
                     # era in sequence.
-                    df_srvy_era_k = slice_DataFrame_rows('Equals', df_srvy_dt, 'Srvy_year', k, 0)  # Defines
+                    df_srvy_era_k = slice_DataFrame_rows('Equals', df_srvy_dt, 'survey_era', k, 0)  # Defines
                     # DataFrame. Calls function. Slices DataFrame to yield single survey era data.
 
                     gdf_srvy_era_k = gpd.GeoDataFrame(df_srvy_era_k,
-                                                      geometry=gpd.points_from_xy(df_srvy_era_k.Easting,
-                                                                                  df_srvy_era_k.Northing),
+                                                      geometry=gpd.points_from_xy(df_srvy_era_k.easting_m,
+                                                                                  df_srvy_era_k.northing_m),
                                                       crs=crdnt_rf_sys)  # Defines GeoDataFrame. Creates shapefile
                     # from DataFrame and coordinate geometry.
 
@@ -191,8 +203,8 @@ for i in rng_nmbrs:  # Begins loop. Loops through array elements. Loops through 
                     gpkg = '/WW_sedimentation_surveys.gpkg'  # Defines variable. Sets name of GeoPackage where
                     # layers will be exported.
 
-                    gdf_srvy_era_k.to_file(otpt_fldr + '/' + gis_fldr + gpkg, layer=lyr_nm, driver='GPKG',
-                                           index=True)  # Saves file to directory.
+                    gdf_srvy_era_k.to_file(otpt_fldr + '/' + gis_fldr + gpkg, layer=lyr_nm, driver='GPKG', index=True)
+                    # Saves file to directory.
 
 # ======================================================================================================================
 # * --------------------------------------------------------------------------------------------------------------------
