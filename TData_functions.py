ef08abc7311744aafd789a465da43813b9edd2e4
wood1466 <wood1466@umn.edu>
2025-01-20 14:23:57 -0600

    Updated to line 164.

--- TData_functions.py
+++ TData_functions.py
@@ -15,16 +15,6 @@ import pandas as pd  # Imports NumPy (the fundamental package for scientific com
 # PART 2: DEFINE FUNCTIONS ---------------------------------------------------------------------------------------------
 
 
-def ForwardRange(Start, End, Step, Display):  # Defines function. For generating forward array between two numbers.
-    End += 1  # Defines variable. Resets end of range so array includes final input value.
-    EndLabel = End - 1  # Defines variable. For display.
-    Range = np.arange(Start, End, Step)  # Defines array.
-    if Display == 1:  # Begins conditional statement. Checks equality. For display.
-        print('\033[1mCREATED ARRAY:\033[0m \n  Limits: ' + str(Start) + ' & ' + str(EndLabel) + ' --> Array:', Range)
-        # Displays objects.
-    return Range  # Ends function execution.
-
-
 def CreateFolder(Path):  # Defines function. For generating directory paths.
     if not os.path.exists(Path):  # Checks if folder exists. Skips step if exists.
         os.mkdir(Path)  # Creates folder if it does not exist.
@@ -41,25 +31,57 @@ def CsvToDataFrame(Path, Display):  # Defines function. For uploading .csv file 
     return df  # Ends function execution.
 
 
-def SliceDataFrameRows(SearchType, Dataframe, Column, Value, Display):  # Defines function. For DataFrame slicing by
-    # row value.
-    if SearchType == 'Equals':  # Begins conditional statement. Checks equality. Sets function format.
-        dfSlice = Dataframe[Dataframe[Column] == Value]  # Defines DataFrame. Slices DataFrame by value and relation.
-    elif SearchType == 'Less than':  # Continues conditional statement. Checks relation. Sets function format.
-        dfSlice = Dataframe[Dataframe[Column] < Value]  # Defines DataFrame. Slices DataFrame by value and relation.
-    elif SearchType == 'Greater than':  # Continues conditional statement. Checks relation. Sets function format.
-        dfSlice = Dataframe[Dataframe[Column] > Value]  # Defines DataFrame. Slices DataFrame by value and relation.
-    elif SearchType == 'Less than/Equal':  # Continues conditional statement. Checks relation. Sets function format.
-        dfSlice = Dataframe[Dataframe[Column] <= Value]  # Defines DataFrame. Slices DataFrame by value and relation.
-    elif SearchType == 'Greater than/Equal':  # Continues conditional statement. Checks relation. Sets function format.
-        dfSlice = Dataframe[Dataframe[Column] >= Value]  # Defines DataFrame. Slices DataFrame by value and relation.
-    else:  # Continues conditional statement. Checks inequality. Sets function format.
-        dfSlice = Dataframe[Dataframe[Column] != Value]  # Defines DataFrame. Slices DataFrame by value and relation.
-    dfSlice = dfSlice.loc[:, ~dfSlice.columns.str.match('Unnamed')]  # Redefines DataFrame. Searches for empty
-    # columns and deletes them.
+def ForwardRange(Start, End, Step, Display):  # Defines function. For generating forward array between two numbers.
+    End += 1  # Defines variable. Resets end of range so array includes final input value.
+    EndLabel = End - 1  # Defines variable. For display.
+    Range = np.arange(Start, End, Step)  # Defines array.
     if Display == 1:  # Begins conditional statement. Checks equality. For display.
-        print('\033[1mCREATED DATAFRAME: \033[0m \n...\n', dfSlice, '\n')  # Displays objects.
-    return dfSlice  # Ends function execution.
+        print('\033[1mCREATED ARRAY:\033[0m \n  Limits: ' + str(Start) + ' & ' + str(EndLabel) + ' --> Array:', Range)
+        # Displays objects.
+    return Range  # Ends function execution.
+
+
+def MoveReferenceCoordinates(x, y, dataframe1, dataframe2, position, column1, column2, column3, conversion_factor,
+                             display):  # Defines function. For moving transect reference coordinates if established
+    # off of line.
+    PointOffsetDistanceFt = SliceDataFrameCell('Float', dataframe1, position, column1, 0)  # Defines variable. Calls
+    # function.  Slices DataFrame to yield marker off transect distance.
+    PointOffsetDistanceM = PointOffsetDistanceFt / conversion_factor  # Defines variable. Converts feet to meters.
+    PointOffsetDirection = SliceDataFrameCell('String', dataframe1, position, column2, 0)  # Defines variable. Calls
+    # function.  Slices DataFrame to yield marker off transect direction.
+    CoordinateAccuracyM = SliceDataFrameCell('String', dataframe2, position, column3, 0)  # Defines variable. Calls
+    # function.  Slices DataFrame to yield marker coordinate estimated accuracy.
+    CoordinateAccuracyMList = CoordinateAccuracyM.split('-')  # Defines list. Splits string into component parts by
+    # delimiter.
+    if len(CoordinateAccuracyMList) == 3:  # Begins conditional statement. Checks equality. Checks if estimated
+        # coordinate accuracy is range.
+        CoordinateAccuracyM = CoordinateAccuracyMList[-1]  # Redefines variable. Selects final list component for
+        # value.
+    else:  # Continues conditional statement. Checks inequality. Checks if estimated coordinate accuracy is a range.
+        CoordinateAccuracyM = CoordinateAccuracyMList[0]  # Redefines variable. Selects first list component for
+        # value.
+    CoordinateAccuracyM = float(CoordinateAccuracyM)  # Redefines variable. Converts data type to float.
+    if PointOffsetDistanceM > CoordinateAccuracyM:  # Begins conditional statement. Checks relation. Moves coordinates
+        # if distance from transect is greater than estimated coordinate accuracy.
+        if PointOffsetDirection == 'North':  # Begins conditional statement. Checks equality. Moves coordinates by
+            # component.
+            y -= PointOffsetDistanceM  # Redefines variable.
+        elif PointOffsetDirection == 'South':  # Continues conditional statement. Checks equality. Moves coordinates by
+            # component.
+            y += PointOffsetDistanceM  # Redefines variable.
+        elif PointOffsetDirection == 'East':  # Continues conditional statement. Checks equality. Moves coordinates by
+            # component.
+            x -= PointOffsetDistanceM  # Redefines variable.
+        elif PointOffsetDirection == 'West':  # Continues conditional statement. Checks equality. Moves coordinates by
+            # component.
+            x += PointOffsetDistanceM  # Redefines variable.
+        else:  # Continues conditional statement. Checks equality. Moves coordinates by component.
+            pass  # Pass command. Moves on to next line of code.f
+    if display == 1:  # Begins conditional statement. Checks equality. For display.
+        print('\033[1mMARKER SHIFT:\033[0m\n Direction: ' + str(PointOffsetDirection) + '\n Magnitude: ' + str(
+            '%.2f' % PointOffsetDistanceM) + '\n Output coordinate: ' + str('%.2f' % x) + ', ' + str('%.2f' % y) + '\n')
+        # Displays objects.
+    return x, y  # Ends function execution.
 
 
 def SliceDataFrameCell(DataType, Dataframe, Position, Column, Display):  # Defines function. For DataFrame slicing by
@@ -83,3 +105,63 @@ def SliceDataFrameCell(DataType, Dataframe, Position, Column, Display):  # Defin
     if Display == 1:  # Begins conditional statement. Checks equality. For display.
         print('\033[1mRETRIEVED VALUE:\033[0m ' + str(CellValue) + '\n')  # Displays objects.
     return CellValue  # Ends function execution.
+
+
+def SliceDataFrameColumns(output, data_type, dataframe, column, check_duplicates, drop_nan, display):  # Defines
+    # function. For DataFrame slicing by column.
+    dfSlice = dataframe[column]  # Defines DataFrame. Slices DatFrame by column header.
+    if check_duplicates == 1:  # Begins conditional statement. Checks equality. Checks if duplicate values should be
+        # removed from DataFrame.
+        dfSlice = dfSlice.drop_duplicates(keep='first')  # Redefines DataFrame. Drops all duplicate values.
+    if drop_nan == 1:  # Begins conditional statement. Checks equality. Checks if NaN values should be removed from
+        # DataFrame.
+        dfSlice = dfSlice.dropna()  # Redefines DataFrame. Drops all NaN values.
+    Type = dfSlice.dtypes  # Defines variable. Retrieves data type of column DataFrame.
+    if Type == data_type:  # Begins conditional statement. Checks equality. Checks if data type is desired by function
+        # input.
+        pass  # Pass command. Moves on to next line.
+    else:  # Continues conditional statement. Checks equality. Checks if data type is desired by function input.
+        if data_type == 'Integer':  # Begins conditional statement. Checks equality. Enforces desired data type.
+            dfSlice = dfSlice.astype(int)  # Redefines DataFrame. Converts values to integer.
+        elif data_type == 'Float':  # Continues conditional statement. Checks equality. Enforces desired data type.
+            dfSlice = dfSlice.astype(float)  # Redefines DataFrame. Converts values to float.
+        else:  # Continues conditional statement. Checks equality. Enforces desired data type.
+            dfSlice = dfSlice.astype(str)  # Redefines DataFrame. Converts value to string.
+    if output == 'DataFrame':  # Begins conditional statement. Checks equality. For output type selection.
+        if display == 1:  # Begins conditional statement. Checks equality. For display.
+            print('\033[1mCREATED DATAFRAME:\033[0m \n...\n', dfSlice, '\n')  # Displays objects.
+        return dfSlice  # Ends function execution.
+    elif output == 'List':  # Continues conditional statement. Checks equality. For output type selection.
+        SliceList = dfSlice.tolist()  # Defines list. Converts DataFrame to list.
+        if display == 1:  # Begins conditional statement. Checks equality. For display.
+            print('\033[1mCREATED LIST:\033[0m', SliceList, '\n')  # Displays objects.
+        return SliceList  # Ends function execution.
+    else:  # Continues conditional statement. Checks equality. For output type selection.
+        SliceArray = dfSlice.to_numpy()  # Defines array. Converts DataFrame to array.
+        if display == 1:  # Begins conditional statement. Checks equality. For display.
+            print('\033[1mCREATED ARRAY:\033[0m', SliceArray, '\n')  # Displays objects.
+        return SliceArray  # Ends function execution.
+
+
+def SliceDataFrameRows(SearchType, Dataframe, Column, Value, Display):  # Defines function. For DataFrame slicing by
+    # row value.
+    if SearchType == 'Equals':  # Begins conditional statement. Checks equality. Sets function format.
+        dfSlice = Dataframe[Dataframe[Column] == Value]  # Defines DataFrame. Slices DataFrame by value and relation.
+    elif SearchType == 'Less than':  # Continues conditional statement. Checks relation. Sets function format.
+        dfSlice = Dataframe[Dataframe[Column] < Value]  # Defines DataFrame. Slices DataFrame by value and relation.
+    elif SearchType == 'Greater than':  # Continues conditional statement. Checks relation. Sets function format.
+        dfSlice = Dataframe[Dataframe[Column] > Value]  # Defines DataFrame. Slices DataFrame by value and relation.
+    elif SearchType == 'Less than/Equal':  # Continues conditional statement. Checks relation. Sets function format.
+        dfSlice = Dataframe[Dataframe[Column] <= Value]  # Defines DataFrame. Slices DataFrame by value and relation.
+    elif SearchType == 'Greater than/Equal':  # Continues conditional statement. Checks relation. Sets function format.
+        dfSlice = Dataframe[Dataframe[Column] >= Value]  # Defines DataFrame. Slices DataFrame by value and relation.
+    else:  # Continues conditional statement. Checks inequality. Sets function format.
+        dfSlice = Dataframe[Dataframe[Column] != Value]  # Defines DataFrame. Slices DataFrame by value and relation.
+    dfSlice = dfSlice.loc[:, ~dfSlice.columns.str.match('Unnamed')]  # Redefines DataFrame. Searches for empty
+    # columns and deletes them.
+    if Display == 1:  # Begins conditional statement. Checks equality. For display.
+        print('\033[1mCREATED DATAFRAME: \033[0m \n...\n', dfSlice, '\n')  # Displays objects.
+    return dfSlice  # Ends function execution.
+
+
+
