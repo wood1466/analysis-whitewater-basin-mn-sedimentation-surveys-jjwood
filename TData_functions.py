2809bac5575d068e2564b8848ca2f9d2c72da2c8
wood1466 <wood1466@umn.edu>
2025-01-22 13:54:01 -0600

    Updated to line 206. Added CalculateTransectAzimuth.

--- TData_functions.py
+++ TData_functions.py
@@ -6,8 +6,11 @@
 # PART 1: INITIALIZATION -----------------------------------------------------------------------------------------------
 
 # IMPORT MODULES -------------------------------------------------------------------------------------------------------
-import os  # Imports os (miscellaneous operating system interfaces). Enables operating system dependent functionality.
+
+import geopandas as gpd  # Imports GeoPandas with alias. Enables geospatial functionality.
+import matplotlib.pyplot as plt  # Imports Matplotlib (visualization with Python) with alias. Enables figure creation.
 import numpy as np  # Imports pandas (Python data analysis library) with alias. Enables use of DataFrames.
+import os  # Imports os (miscellaneous operating system interfaces). Enables operating system dependent functionality.
 import pandas as pd  # Imports NumPy (the fundamental package for scientific computing with Python) with alias. Enables
 # use of advanced mathematics.
 
@@ -15,30 +18,30 @@ import pandas as pd  # Imports NumPy (the fundamental package for scientific com
 # PART 2: DEFINE FUNCTIONS ---------------------------------------------------------------------------------------------
 
 
-def CreateFolder(Path):  # Defines function. For generating directory paths.
-    if not os.path.exists(Path):  # Checks if folder exists. Skips step if exists.
-        os.mkdir(Path)  # Creates folder if it does not exist.
-        print('\033[1mCREATED FOLDER:\033[0m ' + Path + '\n')  # Displays objects.
+def CreateFolder(path):  # Defines function. For generating directory paths.
+    if not os.path.exists(path):  # Checks if folder exists. Skips step if exists.
+        os.mkdir(path)  # Creates folder if it does not exist.
+        print('\033[1mCREATED FOLDER:\033[0m ' + path + '\n')  # Displays objects.
 
 
-def CsvToDataFrame(Path, Display):  # Defines function. For uploading .csv file and converting to a DataFrame for
-    # Python manipulation.
-    CsvData = pd.read_csv(Path)  # Defines variable. Uploads .csv file.
-    df = pd.DataFrame(CsvData)  # Defines DataFrame. Converts .csv. to DataFrame.
-    pd.set_option('display.max_columns', None)  # Adjusts DataFrame display format. Displays all DataFrame columns.
-    if Display == 1:  # Begins conditional statement. Checks equality. For display.
-        print('\033[1mUPLOADED .CSV DATA:\033[0m ' + Path + '\n...\n', df, '\n')  # Displays objects.
-    return df  # Ends function execution.
-
-
-def ForwardRange(Start, End, Step, Display):  # Defines function. For generating forward array between two numbers.
-    End += 1  # Defines variable. Resets end of range so array includes final input value.
-    EndLabel = End - 1  # Defines variable. For display.
-    Range = np.arange(Start, End, Step)  # Defines array.
-    if Display == 1:  # Begins conditional statement. Checks equality. For display.
-        print('\033[1mCREATED ARRAY:\033[0m \n  Limits: ' + str(Start) + ' & ' + str(EndLabel) + ' --> Array:', Range)
+def CreateForwardRange(start, end, step, display):  # Defines function. For generating forward array between two numbers.
+    end += 1  # Defines variable. Resets end of range so array includes final input value.
+    EndLabel = end - 1  # Defines variable. For display.
+    Range = np.arange(start, end, step)  # Defines array.
+    if display == 1:  # Begins conditional statement. Checks equality. For display.
+        print('\033[1mCREATED ARRAY:\033[0m \n  Limits: ' + str(start) + ' & ' + str(EndLabel) + ' --> Array:', Range)
         # Displays objects.
     return Range  # Ends function execution.
+
+
+def ConvertCsvToDataFrame(path, display):  # Defines function. For uploading .csv file and converting to a DataFrame for
+    # Python manipulation.
+    CsvData = pd.read_csv(path)  # Defines variable. Uploads .csv file.
+    df = pd.DataFrame(CsvData)  # Defines DataFrame. Converts .csv. to DataFrame.
+    pd.set_option('display.max_columns', None)  # Adjusts DataFrame display format. Displays all DataFrame columns.
+    if display == 1:  # Begins conditional statement. Checks equality. For display.
+        print('\033[1mUPLOADED .CSV DATA:\033[0m ' + path + '\n...\n', df, '\n')  # Displays objects.
+    return df  # Ends function execution.
 
 
 def MoveReferenceCoordinates(x, y, dataframe1, dataframe2, position, column1, column2, column3, conversion_factor,
@@ -84,25 +87,64 @@ def MoveReferenceCoordinates(x, y, dataframe1, dataframe2, position, column1, co
     return x, y  # Ends function execution.
 
 
-def SliceDataFrameCell(DataType, Dataframe, Position, Column, Display):  # Defines function. For DataFrame slicing by
+def CalculateTransectAzimuth(x1, y1, x2, y2, display):  # Defines function. For transect azimuth calculations from reference coordinates.
+    DeltaX = x2 - x1  # Defines variable. Calculates difference in x coordinates between reference points.
+    DeltaY = y2 - y1  # Defines variable. Calculates difference in y coordinates between reference points.
+    if DeltaX > 0:  # Begins conditional statement. Checks relation. Assigns cartesian quadrant for proper reference angle selection.
+        if DeltaY >= 0:  # Begins conditional statement. Checks relation. Assigns cartesian quadrant for proper reference angle selection.
+            Quadrant = 1  # Defines variable. Assigns quadrant.
+        else:  # Continues conditional statement. Checks relation. Assigns cartesian quadrant for proper reference angle selection.
+            Quadrant = 4  # Defines variable. Assigns quadrant.
+    elif DeltaX < 0:  # Continues conditional statement. Checks relation. Assigns cartesian quadrant for proper reference angle selection.
+        if DeltaY > 0:  # Begins conditional statement. Checks relation. Assigns cartesian quadrant for proper reference angle selection.
+            Quadrant = 2  # Defines variable. Assigns quadrant.
+        else:  # Continues conditional statement. Checks relation.  Assigns cartesian quadrant for proper reference angle selection.
+            Quadrant = 3  # Defines variable. Assigns quadrant.
+    else:  # Continues conditional statement. Checks relation.  Assigns cartesian quadrant for proper reference angle selection.
+        if DeltaY > 0:  # Begins conditional statement. Checks relation.  Assigns cartesian quadrant for proper reference angle selection.
+            Quadrant = 2  # Defines variable. Assigns quadrant.
+        else:  # Continues conditional statement. Checks relation.  Assigns cartesian quadrant for proper reference angle selection.
+            Quadrant = 4  # Defines variable. Assigns quadrant.
+    if Quadrant == 1:  # Begins conditional statement. Checks equality. Calculates transect azimuth.
+        ReferenceAngle = (1/2) * np.pi  # Defines variable. Sets reference angle for correction of calculated orientation to
+        # azimuth.
+        Azimuth = ReferenceAngle - abs(np.arctan(DeltaX / DeltaY))  # Defines variable. Calculates transect azimuth.
+    elif Quadrant == 2:  # Continues conditional statement. Checks equality.  Calculates transect azimuth.
+        ReferenceAngle = (1/2) * np.pi  # Defines variable. Sets reference angle for correction of calculated orientation to
+        # azimuth.
+        Azimuth = ReferenceAngle + abs(np.arctan(DeltaX / DeltaY))  # Defines variable. Calculates transect azimuth.
+    elif Quadrant == 3:  # Continues conditional statement. Checks equality.  Calculates transect azimuth.
+        ReferenceAngle = (3/2) * np.pi  # Defines variable. Sets reference angle for correction of calculated orientation to
+        # azimuth.
+        Azimuth = ReferenceAngle - abs(np.arctan(DeltaX / DeltaY))  # Defines variable. Calculates transect azimuth.
+    else:  # Continues conditional statement. Checks equality.  Calculates transect azimuth.
+        ReferenceAngle = (3/2) * np.pi  # Defines variable. Sets reference angle for correction of calculated orientation to
+        # azimuth.
+        Azimuth = ReferenceAngle + abs(np.arctan(DeltaX / DeltaY))  # Defines variable. Calculates transect azimuth.
+    if display == 1:  # Begins conditional statement. Checks equality. For display.
+        print('\033[1mCALCULATED TRANSECT AZIMUTH:\033[0m ' + str(Azimuth) + '\n')  # Displays objects.
+    return Azimuth  # Ends function execution.
+
+
+def SliceDataFrameCell(data_type, dataframe, position, column, display):  # Defines function. For DataFrame slicing by
     # row value and index.
-    Index = Dataframe.index  # Defines object. Retrieves DataFrame index.
-    if Column is not None:  # Begins conditional statement. Checks inequality. Selects function format.
-        CellValue = Dataframe.loc[Index[Position], Column]  # Defines variable. Slices DatFrame by row value and index.
+    Index = dataframe.index  # Defines object. Retrieves DataFrame index.
+    if column is not None:  # Begins conditional statement. Checks inequality. Selects function format.
+        CellValue = dataframe.loc[Index[position], column]  # Defines variable. Slices DatFrame by row value and index.
     else:  # Continues conditional statement. Checks inequality. Selects function format.
-        CellValue = Dataframe.loc[Index[Position]]  # Defines variable. Slices DatFrame by row value and index.
+        CellValue = dataframe.loc[Index[position]]  # Defines variable. Slices DatFrame by row value and index.
     Type = type(CellValue)  # Defines variable. Retrieves data type of variable.
-    if Type == DataType:  # Begins conditional statement. Checks equality. Checks if data type is desired by function
+    if Type == data_type:  # Begins conditional statement. Checks equality. Checks if data type is desired by function
         # input.
         pass  # Pass command. Moves on to next line.
     else:  # Continues conditional statement. Checks inequality. Checks if data type is desired by function input.
-        if DataType == 'Integer':  # Begins conditional statement. Checks equality. Enforces desired data type.
+        if data_type == 'Integer':  # Begins conditional statement. Checks equality. Enforces desired data type.
             CellValue = int(CellValue)  # Redefines variable. Converts value to integer.
-        elif DataType == 'Float':  # Continues conditional statement. Checks equality. Enforces desired data type.
+        elif data_type == 'Float':  # Continues conditional statement. Checks equality. Enforces desired data type.
             CellValue = float(CellValue)  # Redefines variable. Converts value to float.
         else:  # Continues conditional statement. Checks equality. Enforces desired data type.
             CellValue = str(CellValue)  # Redefines variable. Converts value to string.
-    if Display == 1:  # Begins conditional statement. Checks equality. For display.
+    if display == 1:  # Begins conditional statement. Checks equality. For display.
         print('\033[1mRETRIEVED VALUE:\033[0m ' + str(CellValue) + '\n')  # Displays objects.
     return CellValue  # Ends function execution.
 
@@ -143,25 +185,22 @@ def SliceDataFrameColumns(output, data_type, dataframe, column, check_duplicates
         return SliceArray  # Ends function execution.
 
 
-def SliceDataFrameRows(SearchType, Dataframe, Column, Value, Display):  # Defines function. For DataFrame slicing by
+def SliceDataFrameRows(search_type, dataframe, column, value, display):  # Defines function. For DataFrame slicing by
     # row value.
-    if SearchType == 'Equals':  # Begins conditional statement. Checks equality. Sets function format.
-        dfSlice = Dataframe[Dataframe[Column] == Value]  # Defines DataFrame. Slices DataFrame by value and relation.
-    elif SearchType == 'Less than':  # Continues conditional statement. Checks relation. Sets function format.
-        dfSlice = Dataframe[Dataframe[Column] < Value]  # Defines DataFrame. Slices DataFrame by value and relation.
-    elif SearchType == 'Greater than':  # Continues conditional statement. Checks relation. Sets function format.
-        dfSlice = Dataframe[Dataframe[Column] > Value]  # Defines DataFrame. Slices DataFrame by value and relation.
-    elif SearchType == 'Less than/Equal':  # Continues conditional statement. Checks relation. Sets function format.
-        dfSlice = Dataframe[Dataframe[Column] <= Value]  # Defines DataFrame. Slices DataFrame by value and relation.
-    elif SearchType == 'Greater than/Equal':  # Continues conditional statement. Checks relation. Sets function format.
-        dfSlice = Dataframe[Dataframe[Column] >= Value]  # Defines DataFrame. Slices DataFrame by value and relation.
+    if search_type == 'Equals':  # Begins conditional statement. Checks equality. Sets function format.
+        dfSlice = dataframe[dataframe[column] == value]  # Defines DataFrame. Slices DataFrame by value and relation.
+    elif search_type == 'Less than':  # Continues conditional statement. Checks relation. Sets function format.
+        dfSlice = dataframe[dataframe[column] < value]  # Defines DataFrame. Slices DataFrame by value and relation.
+    elif search_type == 'Greater than':  # Continues conditional statement. Checks relation. Sets function format.
+        dfSlice = dataframe[dataframe[column] > value]  # Defines DataFrame. Slices DataFrame by value and relation.
+    elif search_type == 'Less than/Equal':  # Continues conditional statement. Checks relation. Sets function format.
+        dfSlice = dataframe[dataframe[column] <= value]  # Defines DataFrame. Slices DataFrame by value and relation.
+    elif search_type == 'Greater than/Equal':  # Continues conditional statement. Checks relation. Sets function format.
+        dfSlice = dataframe[dataframe[column] >= value]  # Defines DataFrame. Slices DataFrame by value and relation.
     else:  # Continues conditional statement. Checks inequality. Sets function format.
-        dfSlice = Dataframe[Dataframe[Column] != Value]  # Defines DataFrame. Slices DataFrame by value and relation.
+        dfSlice = dataframe[dataframe[column] != value]  # Defines DataFrame. Slices DataFrame by value and relation.
     dfSlice = dfSlice.loc[:, ~dfSlice.columns.str.match('Unnamed')]  # Redefines DataFrame. Searches for empty
     # columns and deletes them.
-    if Display == 1:  # Begins conditional statement. Checks equality. For display.
+    if display == 1:  # Begins conditional statement. Checks equality. For display.
         print('\033[1mCREATED DATAFRAME: \033[0m \n...\n', dfSlice, '\n')  # Displays objects.
     return dfSlice  # Ends function execution.
-
-
-
