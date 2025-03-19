4e741d7eb7987c48c4a39f8c98ecf71950fd1d4a
Jimmy Wood <107072607+wood1466@users.noreply.github.com>
2025-03-19 17:03:48 -0500

    Update transect_sed_rate_statistics.py
    Updated code to be PEP 8 compliant in relation to object names.

--- transect_sed_rate_statistics.py
+++ transect_sed_rate_statistics.py
@@ -16,20 +16,26 @@ from transect_analysis_functions import *   # Imports all functions from
 # DEFINE INPUT PARAMETERS -----------------------------------------------------
 
 # Directory
-InputFolder = 'Input'  # Defines object for name of input folder where all 
+INPUT_FOLDER = 'Input'  # Defines object for name of input folder where all 
 # input data will be stored.
 
 # Data
-InputFile1 = InputFolder + '/Sedimentation_rates_20250201.csv'  # Defines 
+INPUT_FILE = INPUT_FOLDER + '/Sedimentation_rates_20250201.csv'  # Defines 
 # object for path to input file.
 
 # Operations
-Alpha = 0.05  # Defines object for statistical hypothesis testing level of 
+# Define objects for column names of sample groups in input file.
+
+GROUP_COLUMN_1 = 'SR55_39CmY'
+GROUP_COLUMN_2 = 'SR39_65CmY'
+GROUP_COLUMN_3 = 'SR65_94CmY'
+
+ALPHA = 0.05  # Defines object for statistical hypothesis testing level of 
 # significance.
 
 # UPLOAD FILE(S) --------------------------------------------------------------
 
-dfTSedimentationRates = ConvertCsvToDataFrame(InputFile1, 0)
+df_sed_rates = ConvertCsvToDataFrame(INPUT_FILE, 0)
 # Calls user-defined function (UDF) to define  DataFrame from input data.
 
 # =============================================================================
@@ -42,28 +48,28 @@ dfTSedimentationRates = ConvertCsvToDataFrame(InputFile1, 0)
 # Call UDF to slice DataFrame and define resultant array to serve as sample
 # group for statistical testing.
 
-Ary55_39 = SliceDataFrameColumns(
-    'Array', 'Float', dfTSedimentationRates, 'SR55_39CmY', 0, 1, 0)  
-Ary39_65 = SliceDataFrameColumns(
-    'Array', 'Float', dfTSedimentationRates, 'SR39_65CmY', 0, 1, 0)
-Ary65_94 = SliceDataFrameColumns(
-    'Array', 'Float', dfTSedimentationRates, 'SR65_94CmY', 0, 1, 0)
+sed_rates_55_39 = SliceDataFrameColumns(
+    'Array', 'Float', df_sed_rates, GROUP_COLUMN_1, 0, 1, 0)  
+sed_rates_39_65 = SliceDataFrameColumns(
+    'Array', 'Float', df_sed_rates, GROUP_COLUMN_2, 0, 1, 0)
+sed_rates_65_94 = SliceDataFrameColumns(
+    'Array', 'Float', df_sed_rates, GROUP_COLUMN_3, 0, 1, 0)
 
 # SELECT HYPOTHESIS TESTS -----------------------------------------------------
 
 # Verify sample group normality -----------------------------------------------
 
 # Visually
-LstSampleGroups = [Ary55_39, Ary39_65, Ary65_94]  # Defines list of sample
-# groups for looping visual characterization.
+sample_groups = [sed_rates_55_39, sed_rates_39_65, sed_rates_65_94]
+# Defines list of sample groups for looping visual characterization.
 
-for i in LstSampleGroups:  # Begins through list elements to plot visual
+for i in sample_groups:  # Begins through list elements to plot visual
     # normality check.
-    NumberOfSamplesi = len(i)  # Defines object as group sample size.
+    N_i = len(i)  # Defines object as group sample size.
 
-    # print(NumberOfSamplesi)  # Displays objects.
+    # print(N_i)  # Displays objects.
 
-    # plt.hist(i, bins=int(np.around(np.sqrt(NumberOfSamplesi), decimals=0)))  
+    # plt.hist(i, bins=int(np.around(np.sqrt(N_i), decimals=0)))  
     # Plots histogram.
     # plt.show()  # Shows plot.
     # plt.boxplot(i, meanline=True, showmeans=True)  # Plots box plot.
@@ -72,30 +78,31 @@ for i in LstSampleGroups:  # Begins through list elements to plot visual
     # plt.show()
 
     # Statistically
-    KSTesti = sc.stats.kstest(i, sc.stats.norm.cdf, N=NumberOfSamplesi)  
+    k_s_test_i = sc.stats.kstest(i, sc.stats.norm.cdf, N=N_i)  
     # Defines result of Kolmogorov-Smirnov goodness of fit test for normalilty.
 
-    if KSTesti.pvalue < Alpha:  # Begins conditional statement to check if null
-        # hypothesis is rejected or not.
-        print('Kolmogorov-Smirnov test:\n  P value: ' + str(KSTesti.pvalue)
+    if k_s_test_i.pvalue < ALPHA:  # Begins conditional statement to check if
+        # null hypothesis is rejected or not.
+        print('Kolmogorov-Smirnov test:\n  P value: ' + str(k_s_test_i.pvalue)
               + '\n  Reject that distribution is normal\n')  
         # Displays objects.
     else:
-        print('Kolmogorov-Smirnov test:\n  P value: ' + str(KSTesti.pvalue)
+        print('Kolmogorov-Smirnov test:\n  P value: ' + str(k_s_test_i.pvalue)
               + '\n  Fail to reject that distribution is normal\n')
 
 # Verify sample group homoscedasticity (variance equality) --------------------
 
-BFTest = sc.stats.levene(Ary55_39, Ary39_65, Ary65_94, center='median')  
+b_f_test = sc.stats.levene(
+      sed_rates_55_39, sed_rates_39_65, sed_rates_65_94, center='median')  
 # Defines result of Brown-Forsythe test for variance equality.
 
-if BFTest.pvalue < Alpha:  # Begins conditional statement to check if null
+if b_f_test.pvalue < ALPHA:  # Begins conditional statement to check if null
     # hypothesis is rejected or not.
-    print('Brown-Forsythe test:\n  P value: ' + str(BFTest.pvalue)
+    print('Brown-Forsythe test:\n  P value: ' + str(b_f_test.pvalue)
           + '\n  Reject that sample groups have equal variance\n')
     # Displays objects.
 else:
-    print('Brown-Forsythe test:\n  P value: ' + str(BFTest.pvalue)
+    print('Brown-Forsythe test:\n  P value: ' + str(b_f_test.pvalue)
           + '\n  Fail to reject that sample groups have equal variance\n')
 
 # PERFORM SAMPLE GROUP COMPARISONS --------------------------------------------
@@ -103,46 +110,49 @@ else:
 # Perform nonparametric tests -------------------------------------------------
 
 # Omnibus comparison
-KWTest = sc.stats.kruskal(Ary55_39, Ary39_65, Ary65_94)  # Defines result of
+k_w_ANOVA_test = sc.stats.kruskal(
+      sed_rates_55_39, sed_rates_39_65, sed_rates_65_94)  # Defines result of
 # Kruskal-Wallis H-test for median equality.
 
-if KWTest.pvalue < Alpha:  # Begins conditional statement to check if null
-    # hypothesis is rejected or not.
-    print('Kruskal-Wallis test:\n  P value: ' + str(KWTest.pvalue)
+if k_w_ANOVA_test.pvalue < ALPHA:  # Begins conditional statement to check if
+    # null hypothesis is rejected or not.
+    print('Kruskal-Wallis test:\n  P value: ' + str(k_w_ANOVA_test.pvalue)
           + '\n  Reject that sample groups have equal median\n')
     # Displays objects.
 else:
-    print('Kruskal-Wallis test:\n  P value: ' + str(KWTest.pvalue)
+    print('Kruskal-Wallis test:\n  P value: ' + str(k_w_ANOVA_test.pvalue)
           + '\n  Fail to reject that sample groups have equal median\n')  
 
 # Post-hoc pairwise comparisons
-DnTest = sc_p.posthoc_dunn(LstSampleGroups, p_adjust='bonferroni')  # Defines
+d_test = sc_p.posthoc_dunn(sample_groups, p_adjust='bonferroni')  # Defines
 # result of Dunn's test for median equality.
 
-print("Dunn's test:\n" + str(DnTest) + '\n')  # Displays objects.
+print("Dunn's test:\n" + str(d_test) + '\n')  # Displays objects.
 
 # Perform parametric tests ----------------------------------------------------
 
 # Assume normality and equal variance -----------------------------------------
 
 # Omnibus comparison
-ANOVATest = sc.stats.f_oneway(Ary55_39, Ary39_65, Ary65_94)  # Defines result
-# of one-way ANOVA test for mean equality.
+ANOVA_test = sc.stats.f_oneway(
+    sed_rates_55_39, sed_rates_39_65, sed_rates_65_94)  # Defines result of
+# one-way ANOVA test for mean equality.
 
-if ANOVATest.pvalue < Alpha:  # Begins conditional statement to check if null
+if ANOVA_test.pvalue < ALPHA:  # Begins conditional statement to check if null
     # hypothesis is rejected or not.
-    print('One-way ANOVA test:\n  P value: ' + str(ANOVATest.pvalue)
+    print('One-way ANOVA test:\n  P value: ' + str(ANOVA_test.pvalue)
           + '\n  Reject that sample groups have equal mean\n')
     # Displays objects.
 else:
-    print('One-way ANOVA test:\n  P value: ' + str(ANOVATest.pvalue)
+    print('One-way ANOVA test:\n  P value: ' + str(ANOVA_test.pvalue)
           + '\n  Fail to reject that sample groups have equal mean\n')  
 
 # Post-hoc pairwise comparisons
-TkTest = sc.stats.tukey_hsd(Ary55_39, Ary39_65, Ary65_94)  # Defines result of
+t_HSD_test = sc.stats.tukey_hsd(
+    sed_rates_55_39, sed_rates_39_65, sed_rates_65_94)  # Defines result of
 # Tukey's HSD test for mean equality.
 
-print("Tukey's test:\n" + str(TkTest) + '\n')  # Displays objects.
+print("Tukey's test:\n" + str(t_HSD_test) + '\n')  # Displays objects.
 
 # Assume normality and unequal variance ---------------------------------------
 
@@ -150,35 +160,36 @@ print("Tukey's test:\n" + str(TkTest) + '\n')  # Displays objects.
 
 # Define DataFrame of sample group.
 
-df55_39 = pd.DataFrame({'Samples': Ary55_39, 'Group': 1})
-df39_65 = pd.DataFrame({'Samples': Ary39_65, 'Group': 2})
-df65_94 = pd.DataFrame({'Samples': Ary65_94, 'Group': 3})
+df_sed_rates_55_39 = pd.DataFrame({'Samples': sed_rates_55_39, 'Group': 1})
+df_sed_rates_39_65 = pd.DataFrame({'Samples': sed_rates_39_65, 'Group': 2})
+df_sed_rates_65_94 = pd.DataFrame({'Samples': sed_rates_65_94, 'Group': 3})
 
-dfSampleGroups = pd.concat([df55_39, df39_65, df65_94])  # Defines DataFrame
-# through concatenation.
+df_sample_groups = pd.concat([
+    df_sed_rates_55_39, df_sed_rates_39_65, df_sed_rates_65_94])
+# Defines DataFrame through concatenation.
 
 # Omnibus comparison
-WlANOVATest = pg.welch_anova(
-    data=dfSampleGroups, dv='Samples', between='Group')  # Defines result of
+w_ANOVA_test = pg.welch_anova(
+    data=df_sample_groups, dv='Samples', between='Group')  # Defines result of
 # One-way Welch's ANOVA test for mean equality.
 
-if WlANOVATest.iloc[0, 4] < Alpha:  # Begins conditional statement to check if
+if w_ANOVA_test.iloc[0, 4] < ALPHA:  # Begins conditional statement to check if
     # null hypothesis is rejected or not.
     print("One-way Welch's ANOVA test:\n  P value: "
-          + str(WlANOVATest.iloc[0, 4])
+          + str(w_ANOVA_test.iloc[0, 4])
           + '\n  Reject that sample groups have equal mean\n')
     # Displays objects.
 else:
     print("One-way Welch's ANOVA test:\n  P value: "
-          + str(WlANOVATest.iloc[0, 4])
+          + str(w_ANOVA_test.iloc[0, 4])
           + '\n  Fail to reject that sample groups have equal mean\n')
 
 # Post-hoc pairwise comparisons
-GHTest = pg.pairwise_gameshowell(
-    data=dfSampleGroups, dv='Samples', between='Group')  # Defines result of
+g_h_test = pg.pairwise_gameshowell(
+    data=df_sample_groups, dv='Samples', between='Group')  # Defines result of
 # Games-Howell (nonparametric) test for mean equality.
 
-print('Games-Howell test:\n' + str(GHTest) + '\n')  # Displays objects.
+print('Games-Howell test:\n' + str(g_h_test) + '\n')  # Displays objects.
 
 # SIGNAL END ==================================================================
 
